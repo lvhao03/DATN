2450902894952d155f3c429cf0d14ae815fb5336
Your Name <vuongtai0403@gmail.com>
2024-02-23 12:56:28 +0700

    tai deptrai

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,11 @@ Route::get('/shop', function () {
 Route::get('/detail', function () {
     return view('client.detail');
 });
+
+
+Route::get('/detail_blog', function () {
+    return view('client.detail_blog');
+});
 Route::get('/detail/{id}', [ProductController::class,'detail']);
 Route::get('/themvaogio/{idsp}/{soluong}', [ProductController::class,'themvaogio']);
 Route::get('/hiengiohang', [ProductController::class,'hiengiohang']);
